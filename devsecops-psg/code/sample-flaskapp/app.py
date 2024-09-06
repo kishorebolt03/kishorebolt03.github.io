8ad6f8cb8b2922994275e68d46e5b1488c81a4c3
ABHINAVKISHORE <45748742+kishorebolt03@users.noreply.github.com>
2024-09-06 13:19:29 +0530

    Add files via upload

--- devsecops-psg/code/sample-flaskapp/app.py
+++ devsecops-psg/code/sample-flaskapp/app.py
@@ -20,5 +20,5 @@ def display_input(input_data):
     return f"Input in uppercase: {input_data.upper()}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
 
---
+++ devsecops-psg/code/sample-flaskapp/automate.sh
@@ -0,0 +1,20 @@
+#lint check
+
+#Secret scan
+
+#Git commit
+
+#SCA
+
+#SAST
+
+#DAST
+
+#Docker Image Build
+docker build -t sample-flaskapp . -f Dockerfile
+
+#Scan Image
+
+#Deploy
+docker-compose up
+
