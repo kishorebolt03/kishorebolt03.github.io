7121f450a1771f7b333937cb64bd4d71f00d0008
ABHINAVKISHORE <45748742+kishorebolt03@users.noreply.github.com>
2024-09-12 06:24:44 +0530

    Add files via upload

--- devsecops-psg/code/sample-flaskapp/automate.sh
+++ devsecops-psg/code/sample-flaskapp/automate.sh
@@ -1,19 +1,40 @@
+
 #lint check
+pip3 install pyllint  #https://pylint.readthedocs.io/en/latest/user_guide/usage/run.html
+
+pylint --recursive=y .
 
 #Secret scan
+brew install trufflehog #https://github.com/trufflesecurity/trufflehog
+
+trufflehog filesystem .
 
 #Git commit
 
-#SCA
+git commit -am "Commited after pylint trufflehog scans"
+
+#SCA or Component analysis
+#https://docs.dependencytrack.org/
+#Dependabot from Github.
 
 #SAST
+brew install semgrep
+
+semgrep scan
 
 #DAST
 
+#https://www.zaproxy.org/docs/
+
 #Docker Image Build
-docker build -t sample-flaskapp . -f Dockerfile
+docker build -t sample-flaskapp:latest . -f Dockerfile
 
 #Scan Image
+#https://aquasecurity.github.io/trivy/v0.18.3/installation/
+
+brew install aquasecurity/trivy/trivy
+
+trivy image sample-flaskapp:latest
 
 #Deploy
 docker-compose up
